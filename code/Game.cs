ef726e839801e866d9aac6da730ecd6212cd6ce7
SleepyMode <28950192+SleepyMode@users.noreply.github.com>
2023-02-25 23:17:29 +0200

    general: Initial project setup

---
+++ code/Game.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storm;
+
+public partial class MyGame : GameManager
+{
+	public MyGame()
+	{
+	}
+
+	public override void ClientJoined( IClient client )
+	{
+		base.ClientJoined( client );
+
+		var pawn = new Pawn();
+		client.Pawn = pawn;
+
+		var spawnpoints = Entity.All.OfType<SpawnPoint>();
+
+		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		if ( randomSpawnPoint != null )
+		{
+			var tx = randomSpawnPoint.Transform;
+			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
+			pawn.Transform = tx;
+		}
+	}
+}
