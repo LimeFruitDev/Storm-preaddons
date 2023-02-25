558a193997636f687dc911915b48eca53e99b8c9
SleepyMode <28950192+SleepyMode@users.noreply.github.com>
2023-02-25 23:47:35 +0200

    events: Create events for game functions

---
+++ code/Events/GameEvents.cs
@@ -0,0 +1,41 @@
+﻿namespace Sandbox;
+
+public static partial class StormEvent
+{
+	public static string Initialize = "Initialize";
+	public static string Shutdown = "Shutdown";
+	public static string PostLevelLoaded = "PostLevelLoaded";
+	public static string RenderHud = "RenderHud";
+	public static string ClientJoined = "ClientJoined";
+	public static string ClientDisconnect = "ClientDisconnect";
+
+	public class InitializeAttribute : EventAttribute
+	{
+		public InitializeAttribute() : base( Initialize ) { }
+	}
+
+	public class ShutdownAttribute : EventAttribute
+	{
+		public ShutdownAttribute() : base( Shutdown ) { }
+	}
+
+	public class PostLevelLoadedAttribute : EventAttribute
+	{
+		public PostLevelLoadedAttribute() : base( PostLevelLoaded ) { }
+	}
+
+	public class RenderHudAttribute : EventAttribute
+	{
+		public RenderHudAttribute() : base( RenderHud ) { }
+	}
+
+	public class ClientJoinedAttribute : EventAttribute
+	{
+		public ClientJoinedAttribute() : base( ClientJoined ) { }
+	}
+
+	public class ClientDisconnectAttribute : EventAttribute
+	{
+		public ClientDisconnectAttribute() : base( ClientDisconnect ) { }
+	}
+}
--- code/Game.cs
+++ code/Game.cs
@@ -1,6 +1,4 @@
-﻿using Sandbox;
-
-namespace Sandbox;
+﻿namespace Sandbox;
 
 public partial class StormGame : GameManager
 {
@@ -8,10 +6,41 @@ public partial class StormGame : GameManager
 
 	public StormGame()
 	{
+		Event.Run( StormEvent.Initialize );
+	}
+
+	public override void Shutdown()
+	{
+		Event.Run( StormEvent.Shutdown );
+
+		base.Shutdown();
+	}
+
+	public override void PostLevelLoaded()
+	{
+		Event.Run( StormEvent.PostLevelLoaded );
+
+		base.PostLevelLoaded();
+	}
+
+	public override void RenderHud()
+	{
+		Event.Run( StormEvent.RenderHud );
+
+		base.RenderHud();
 	}
 
 	public override void ClientJoined( IClient client )
 	{
+		Event.Run( StormEvent.ClientJoined, client );
+
 		base.ClientJoined( client );
+	}
+
+	public override void ClientDisconnect( IClient client, NetworkDisconnectionReason reason )
+	{
+		Event.Run( StormEvent.ClientDisconnect, client, reason );
+
+		base.ClientDisconnect( client, reason );
 	}
 }
