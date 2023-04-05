924c621bae275d8ff582691fef27152298e3fb92
SleepyMode <28950192+SleepyMode@users.noreply.github.com>
2023-04-05 16:28:55 +0300

    factions: Create BaseFaction class

---
+++ code/Framework/Factions/BaseFaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Sandbox;
+
+namespace Storm;
+
+public partial class BaseFaction : BaseNetworkable
+{
+	[Net] public string UniqueId { get; set; }
+	[Net] public string Name { get; set; }
+	[Net] public string Description { get; set; }
+	[Net] public bool IsWhitelisted { get; set; }
+	[Net] public IList<string> Models { get; set; }
+	[Net] public IList<Player> Players { get; set; }
+}
