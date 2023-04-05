8d24de257b7ffa30685f81af2b9ee4957f4eb91c
SleepyMode <28950192+SleepyMode@users.noreply.github.com>
2023-04-05 16:20:43 +0300

    schema: Add an (empty) schema

---
+++ code/Schema/README.md
@@ -0,0 +1,5 @@
+
+# This is where your schema files go!
+Welcome to Storm!
+
+This folder provides a sample schema implementation to show you how to easily create a new schema on Storm.
---
+++ code/Schema/SampleSchema.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Storm;
+
+public class SampleSchema : BaseSchema
+{
+	public override void Initialize()
+	{
+		Log.Info( "Hello, welcome to my schema!" );
+		Log.Info( $"Schema Name: {Details.Name}" );
+		Log.Info( $"Schema Author: {Details.Author}" );
+		Log.Info( $"Schema Description: {Details.Description}" );
+	}
+}
